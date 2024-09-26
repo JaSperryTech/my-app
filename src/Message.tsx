b2cdbf30464947e04b3cdeb56fea15dc7a5885dc
Jace Sperry <jsperry@gmail.com>
2024-09-26 10:32:05 -0400

    3.9 - Passing Functions via Props

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+
+// PascalCasing
+function Message() {
+    // JSX: Javascript XML
+    const name = "Jace";
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>;
+}
+
+export default Message;
