7f903987d3829f6220a697243a7993cb6da6babe
Jace Sperry <jsperry@gmail.com>
2024-10-01 09:50:42 -0400

    4.9 - Exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { BsFillCalendarFill } from "react-icons/bs";
+import Button from "./components/Button";
 
 function App() {
-
   return (
     <div>
-      <BsFillCalendarFill color="red" size={40}/>
+      <Button onClick={() => {console.log("Clicked")}}>My Button</Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,3 +1,5 @@
+import styles from "./Button.module.css"
+
 interface Props {
   children: string;
   color?: "primary" | "secondary" | "danger";
@@ -6,7 +8,7 @@ interface Props {
 
 const Button = ({ children, onClick, color = "primary" }: Props) => {
   return (
-    <div className={"btn btn-" + color} onClick={onClick}>
+    <div className={[styles.btn, styles['btn-' + color]].join(" ")} onClick={onClick}>
       {children}
     </div>
   );
