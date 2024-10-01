c90579d2dda0c4713ed37ba2c9b56b77c8b767be
Jace Sperry <jsperry@gmail.com>
2024-10-01 09:19:53 -0400

    4.8 - Icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import ListGroup from "./components/ListGroup";
+import { BsFillCalendarFill } from "react-icons/bs";
 
 function App() {
-  const items = ["New York", "Los Angeles", "San Angeles"]
 
   return (
     <div>
-      <ListGroup heading="Miami" items={items} onSelectItem={() => console.log(items)} />
+      <BsFillCalendarFill color="red" size={40}/>
     </div>
   );
 }
