936a069ce8c1f03cdd716457b486b308de18bd68
Jace Sperry <jsperry@gmail.com>
2024-10-02 09:30:33 -0400

    5.4 - Pure Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
-import { useState } from "react";
+import Message from "./Message";
 
 const App = () => {
-  // const [firstName, setFirstName] = useState("");
-  // const [lastName, setLastName] = useState("");
-  const [person, setPerson] = useState({
-    firstName: "",
-    lastName: "",
-  });
-  const [isLoading, setLoading] = useState(false);
-
-  return <div></div>;
+  return (
+    <div>
+      <Message />
+      <Message />
+      <Message />
+    </div>
+  );
 };
 
 export default App;
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,11 +1,8 @@
+let count = 0;
 
-// PascalCasing
-function Message() {
-    // JSX: Javascript XML
-    const name = "Jace";
-    if (name)
-        return <h1>Hello {name}</h1>;
-    return <h1>Hello World</h1>;
-}
+const Message = () => {
+  count++;
+  return <div>Message {count}</div>;
+};
 
 export default Message;
