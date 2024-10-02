c57bb8a44c58f40225d27f489a13dde395f3d334
Jace Sperry <jsperry@gmail.com>
2024-10-02 09:52:51 -0400

    5.6 - Updating Objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import Message from "./Message";
+import { useState } from "react";
 
-const App = () => {
+function App() {
+  const [drink, setDrink] = useState({
+    title: "Americano",
+    price: 5,
+  });
+
+  const handleClick = () => {
+    setDrink({ ...drink, price: 6 });
+  };
+
   return (
     <div>
-      <Message />
+      {drink.price} <button onClick={handleClick}>Click Me</button>;
     </div>
   );
-};
-
+}
 export default App;
