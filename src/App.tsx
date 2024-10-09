b2a59b0ec10d9cdebe444c683495a420c31ffe85
Jace Sperry <jsperry@gmail.com>
2024-10-09 08:18:00 -0400

    7.7 - handling Errors

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,23 @@ interface User {
 
 const App = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setUsers(res.data));
+      .then((res) => setUsers(res.data))
+      .catch((err) => setError(err.message));
   }, []);
   return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
+    <>
+      <p className="text-danger">{error}</p>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
   );
 };
 
